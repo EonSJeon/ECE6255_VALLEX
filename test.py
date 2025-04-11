a68815aace85e1801a02d98fe85e237200ccac35
Sangeon Jeon <sjeon84@atl1-1-02-009-32-0.pace.gatech.edu>
2025-04-11 17:17:43 -0400

    Your commit message describing the changes

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import torch
 import logging
@@ -71,23 +70,3 @@ def get_model(device):
     vocos = Vocos.from_pretrained('charactr/vocos-encodec-24khz').to(device)
     
     return model, codec, vocos
-=======
-from utils.prompt_making import make_prompt
-
-### Use given transcript
-make_prompt(name="sample", audio_prompt_path="sample.wav",
-                transcript="It was a bright cold day in April, and the clocks were striking thirteen.")
-
-from utils.generation import SAMPLE_RATE, generate_audio, preload_models
-from scipy.io.wavfile import write as write_wav
-
-# download and load all models
-preload_models()
-
-text_prompt = """
-Hey, Traveler, Listen to this, This machine has taken my voice, and now it can talk just like me!
-"""
-audio_array = generate_audio(text_prompt, prompt="sample")
-
-write_wav("sample_cloned.wav", SAMPLE_RATE, audio_array)
->>>>>>> 9c5cba6e7226898c0cfa6441237eec64ae201095
