7700dceb29596e153ee3f08e134f6d7b8e8adbae
EonSJeon <jeonsangeon55@gmail.com>
2025-04-30 19:25:56 -0400

    rr

--- gen.py
+++ gen.py
@@ -1,24 +1,36 @@
-from utils.prompt_making import make_prompt
-from train_utils.icefall.utils import load_checkpoint
+# from utils.prompt_making import make_prompt
+# from train_utils.icefall.utils import load_checkpoint
 
-### Use given transcript
-make_prompt(name="indian", audio_prompt_path="indian_accent.wav",
-                transcript="not wanted my weaknesses to be detected by Bhabaprasad")
+# ### Use given transcript
+# make_prompt(name="indian", audio_prompt_path="sample_indian.wav",
+#                 transcript="not wanted my weaknesses to be detected by Bhabaprasad")
 
-from utils.generation import SAMPLE_RATE, generate_audio, preload_models
-from scipy.io.wavfile import write as write_wav
-import utils.generation as gen
+# from utils.generation import SAMPLE_RATE, generate_audio, preload_models
+# from scipy.io.wavfile import write as write_wav
+# import utils.generation as gen
 
-# download and load all models
-gen.preload_models()
+import pandas as pd
 
-filename = "./checkpoints/vallex-checkpoint.pt"
+df = pd.read_csv("./test_dataset/en/english_transcripts.csv")
 
-load_checkpoint(filename, gen.model, None, None, None)
+for index, row in df.iterrows():
+    print(row['id'])
+    print(row['text'])
+    # print(row['romaji'])
+    # print(row['english'])
+    print("--------------------------------")
 
-text_prompt = """
-not wanted my weaknesses to be detected by Bhabaprasad
-"""
-audio_array = gen.generate_audio(text_prompt, prompt="indian", language='en', accent='English')
+# # download and load all models
+# gen.preload_models()
 
-write_wav("sample_indian.wav", SAMPLE_RATE, audio_array)
+# pure_checkpoint = "./checkpoints/checkpoint-30-pure.pt"
+# cloned_checkpoint = "./checkpoints/checkpoint-30-cloned.pt"
+
+# load_checkpoint(pure_checkpoint, gen.model, None, None, None)
+
+# text_prompt = """
+# not wanted my weaknesses to be detected by Bhabaprasad"
+# """
+# audio_array = gen.generate_audio(text_prompt, prompt="indian", language='en', accent='English')
+
+# write_wav("sample_indian_cloned-pure.wav", SAMPLE_RATE, audio_array)
